8380930dcb561be7eef126cee5687bfb36e8bf55
Amady99 <diopamady95@gmail.com>
2021-08-02 10:50:18 +0200

    stash

--- fichier1.py
+++ fichier1.py
@@ -3,4 +3,8 @@ numero_de_utilisateur = 2
 print(f"Hello, je suis le fichier {numero_de_fichier}")
 print(f"Je suis l'utilisateur {numero_de_utilisateur}")
 
+<<<<<<< HEAD
 print("Un super messages")
+=======
+print("Un message encore meilleur")
+>>>>>>> 8f0540f5d8abe7a30fd9544124db3c7646714424
